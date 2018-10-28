bf5af97614e94e0227389f2155f686829530276b
laschuet <laschuet@users.noreply.github.com>
2018-10-28 23:19:30 +0100

    Add new default colorscheme

--- src/Avatar.jl
+++ src/Avatar.jl
@@ -3,7 +3,7 @@ module Avatar
 using Images
 
 # (? ” ?)
-# http://www.colourlovers.com/palette/848743
+# https://www.colourlovers.com/palette/848743
 p848743 = [
     RGB(73 / 255, 10 / 255, 61 / 255),
     RGB(189 / 255, 21 / 255, 80 / 255),
@@ -13,7 +13,7 @@ p848743 = [
 ]
 
 # Compatible
-# http://www.colourlovers.com/palette/932683
+# https://www.colourlovers.com/palette/932683
 p932683 = [
     RGB(63 / 255, 184 / 255, 175 / 255),
     RGB(127 / 255, 199 / 255, 175 / 255),
@@ -22,9 +22,19 @@ p932683 = [
     RGB(255 / 255, 61 / 255, 127 / 255)
 ]
 
+# you are beautiful
+# https://www.colourlovers.com/palette/379413
+p379413 = [
+    RGB(53 / 255, 19 / 255, 48 / 255),
+    RGB(66 / 255, 66 / 255, 84 / 255),
+    RGB(100 / 255, 144 / 255, 138 / 255),
+    RGB(232 / 255, 202 / 255, 164 / 255),
+    RGB(202 / 255, 42 / 255, 65 / 255)
+]
+
 """"""
 function generate(cells_x=5, cells_y=5, cell_size_x=80, cell_size_y=80,
-                    palette=p932683, out="avatar.png")
+                    palette=p379413, out="avatar.png")
     CELL1 = fill(palette[1], cell_size_x, cell_size_y)
     CELL2 = fill(palette[2], cell_size_x, cell_size_y)
     CELL3 = fill(palette[3], cell_size_x, cell_size_y)
