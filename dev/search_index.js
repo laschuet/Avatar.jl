767f548528e50f04c61922763eabe1d4336d3e1a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-27 17:04:48 +0000

    build based on 2f3b170

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Avatar.jl-1",
+    "page": "Home",
+    "title": "Avatar.jl",
+    "category": "section",
+    "text": ""
+},
+
+]}
